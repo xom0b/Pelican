b933055149235a8b35d72e6357197c60eb3cf560
BOGOSLAF\Henry <henry.schaefer@outlook.com>
2019-01-08 18:48:01 -0800

    i don't even remember what the hell i was doin but i did somethin

---
+++ Unity Project/Pelican Game/Assets/Scenes/Scripts/FitBoxColliderToSprite.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FitBoxColliderToSprite : MonoBehaviour
+{
+    public BoxCollider2D boxCollider;
+    public SpriteRenderer sprite;
+    
+    public void ResizeBoxColliderToSprite()
+    {
+        if (boxCollider != null && sprite != null)
+        {
+            Vector2 spriteSize = sprite.bounds.size;
+            boxCollider.size = new Vector2(spriteSize.x / sprite.transform.lossyScale.x, spriteSize.y / sprite.transform.lossyScale.y);
+            boxCollider.offset = new Vector2(0f, 0f);
+        }
+    }
+}
